60eceef2d30308981d3f4b82371e1eaaa843b5b3
NeoTorino <neotorino13@gmail.com>
2025-06-18 19:21:11 +0100

    combined api and search routes

--- app_factory/route_setup.py
+++ app_factory/route_setup.py
@@ -1,6 +1,5 @@
 from routes.main_routes import main
 from routes.utility_routes import utility
-from routes.api_routes import api
 from routes.page_routes import pages
 from routes.search_routes import search
 
@@ -9,6 +8,5 @@ def register_blueprints(app):
 
     app.register_blueprint(main)
     app.register_blueprint(pages)
-    app.register_blueprint(search)
     app.register_blueprint(utility)
-    app.register_blueprint(api)
+    app.register_blueprint(search)
